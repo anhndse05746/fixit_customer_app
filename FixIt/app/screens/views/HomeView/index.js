2a0ea2b4d28217210035c70a761b76769a14745b
anhndse05746 <anhnd128@gmail.com>
2021-03-11 03:57:19 +0700

    get major list

--- FixIt/app/screens/views/HomeView/index.js
+++ FixIt/app/screens/views/HomeView/index.js
@@ -1,74 +1,88 @@
-import React from 'react';
-import {FlatList, SafeAreaView} from 'react-native';
-import {View} from 'react-native';
-import {StyleSheet, Text} from 'react-native';
-import {calcScale} from '../../../utils/dimension';
+import React, { useEffect } from 'react';
+import { FlatList, SafeAreaView } from 'react-native';
+import { View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { calcScale } from '../../../utils/dimension';
 import HeaderBar from './HeaderBar';
 import CommonStyles from '../Styles';
 import ServiceItem from './ServiceItem';
-import {useNavigation} from '@react-navigation/native';
-import {useSelector} from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
+import { useDispatch, useSelector } from 'react-redux';
 
-const primaryServices = [
-  {
-    image: require('../../../assets/images/freezing.png'),
-    id: '1',
-    name: 'Sửa điện lạnh',
-    url: '',
-    parentId: 0,
-    description: 'Sửa tủ lạnh, điều hòa, máy giặt,...',
-    backgroundColor: '#f2552c',
-  },
-  {
-    image: require('../../../assets/images/laptop.png'),
-    id: '2',
-    name: 'Sửa điện tử',
-    url: '',
-    parentId: 0,
-    description: 'Sửa laptop, điện thoại...',
-    backgroundColor: '#ffd15c',
-  },
-  {
-    image: require('../../../assets/images/house.png'),
-    id: '3',
-    name: 'Sửa đồ gia dụng',
-    url: '',
-    parentId: 0,
-    description: 'Sửa tủ, giường...',
-    backgroundColor: '#00a66f',
-  },
-  {
-    image: require('../../../assets/images/lightbulb.png'),
-    id: '4',
-    name: 'Sửa điện dân dụng',
-    url: '',
-    parentId: 0,
-    description: 'Sửa đèn, điện...',
-    backgroundColor: '#098eb3',
-  },
-];
+import { loadMajors } from '../../../store/majors'
+
+// const primaryServices = [
+//   {
+//     image: require('../../../assets/images/freezing.png'),
+//     id: '1',
+//     name: 'Sửa điện lạnh',
+//     url: '',
+//     parentId: 0,
+//     description: 'Sửa tủ lạnh, điều hòa, máy giặt,...',
+//     backgroundColor: '#f2552c',
+//   },
+//   {
+//     image: require('../../../assets/images/laptop.png'),
+//     id: '2',
+//     name: 'Sửa điện tử',
+//     url: '',
+//     parentId: 0,
+//     description: 'Sửa laptop, điện thoại...',
+//     backgroundColor: '#ffd15c',
+//   },
+//   {
+//     image: require('../../../assets/images/house.png'),
+//     id: '3',
+//     name: 'Sửa đồ gia dụng',
+//     url: '',
+//     parentId: 0,
+//     description: 'Sửa tủ, giường...',
+//     backgroundColor: '#00a66f',
+//   },
+//   {
+//     image: require('../../../assets/images/lightbulb.png'),
+//     id: '4',
+//     name: 'Sửa điện dân dụng',
+//     url: '',
+//     parentId: 0,
+//     description: 'Sửa đèn, điện...',
+//     backgroundColor: '#098eb3',
+//   },
+// ];
 
 const HomeView = () => {
-  let data = useSelector((state) => state.user);
+  const data = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigation = useNavigation();
+
+  //select user's token
+  const { token } = data
+
+  //select majorList
+  const { majorsList } = useSelector(state => state.majors)
+
+  useEffect(() => {
+    dispatch(loadMajors(token))
+  }, [])
+
   return (
     <SafeAreaView style={styles.container}>
       <HeaderBar navigation={navigation} />
       <View style={styles.innerContainer}>
-        <Text style={[styles.textBold, {paddingTop: calcScale(80)}]}>
+        <Text style={[styles.textBold, { paddingTop: calcScale(80) }]}>
           Hi, {data.name}!
         </Text>
         <Text style={styles.textRegular}>
           Hôm nay bạn cần sửa chữa gì không?
         </Text>
         <FlatList
-          data={primaryServices}
+          data={majorsList}
           style={styles.serviceContainer}
           renderItem={(item) => (
             <ServiceItem navigation={navigation} item={item} />
           )}
           numColumns={2}
-          columnWrapperStyle={{flex: 1, justifyContent: 'space-around'}}
+          columnWrapperStyle={{ flex: 1, justifyContent: 'space-around' }}
           keyExtractor={(item) => item.id}
         />
       </View>
--- FixIt/app/store/reducers.js
+++ FixIt/app/store/reducers.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import app from './appState';
 import user from './user'
 import register from './register'
+import majors from './majors'
 
 export default combineReducers({
     app,
     user,
-    register
+    register,
+    majors
 });
